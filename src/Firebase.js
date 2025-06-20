cf9df8cb4e4e411fec545ec8b61c76e98f73489c
seerat-00 <iseeratsharmash@gmail.com>
2025-06-20 17:03:48 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,26 +2,24 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// Firebase project configuration object
+// Firebase config from environment variables
 const firebaseConfig = {
-  apiKey: "", 
-  authDomain: "", 
-  projectId: "", 
-  storageBucket: "", 
-  messagingSenderId: "", 
-  appId: "" 
-}; //empty for security reason
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
 
-// Initialize Firebase app with config object
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-console.log('Firebase app initialized:', app.name); // Log Firebase app name (usually '[DEFAULT]')
+console.log('Firebase app initialized:', app.name);
 
-// Initialize Firebase Authentication and Firestore services
 const auth = getAuth(app);
 console.log('Firebase Auth service initialized.');
 
 const db = getFirestore(app);
 console.log('Firestore database service initialized.');
 
-// Export initialized services for use in other parts of the app
 export { app, auth, db };
